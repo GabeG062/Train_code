a9738762734e52e1f2a3502698a3a88d4c5672aa
starman062 <gabrielgross222@gmail.com>
2026-06-03 16:04:53 -0700

    Added missing .csproj

---
+++ Train_Code/Train.cs
@@ -0,0 +1,11 @@
+namespace DominoClasses
+{
+    public abstract class Train
+    {
+    //fields
+    private List<Domino> dominoes = new List<Domino>();
+    //properties
+
+    //methods
+    }
+}
