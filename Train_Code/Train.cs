73f83a9bcdd4e2d695ab5c80fbcc65cc4692a2d4
starman062 <gabrielgross222@gmail.com>
2026-06-03 19:50:26 -0700

    Baseline for Train.cs done

--- Train_Code/Train.cs
+++ Train_Code/Train.cs
@@ -4,8 +4,87 @@ namespace DominoClasses
     {
     //fields
     private List<Domino> dominoes = new List<Domino>();
+    
+    private int engineValue;
     //properties
 
+    private int Count
+    {
+        get
+        {
+            return dominoes.Count;
+        }
+    }
+
+    public int EngineValue
+    {
+        get => engineValue;
+        set => engineValue = value;
+    }
+
+    public bool IsEmpty
+    {
+        get
+        {
+            return Count == 0;
+        }
+    }
+
+    public Domino LastDomino
+    {
+        get
+        {
+            return dominoes[Count];
+        }
+    }
+
+    public int PlayableValue
+    {
+        get
+        {
+            return 0;
+        }
+    }
+
+    public Domino this[int index]
+    {
+        get
+        {
+            return dominoes[index];
+        }
+    }
+        
     //methods
+    public void Add(Domino d)
+    {
+        dominoes.Add(d);
+    }
+
+    public bool IsPlayable(Domino d, out bool mustFlip)
+    {
+        mustFlip = false;
+        return true;
+    }
+
+    public abstract bool IsPlayable(Hand h, Domino d, out bool mustFlip);
+
+    public void Play(Hand h, Domino d)
+    {
+        return;
+    }
+
+    public Train() {}
+
+    public Train(int engineValue)
+    {
+        this.engineValue = engineValue;
+    }
+
+    public override string ToString()
+    {
+        return "Train";
+    }
+    
+    
     }
 }
